96c9e0fd4ae4203f8284c81f7d38d679c68b0957
Ashutosh <ashutoshranghar1@gmail.com>
2025-11-25 11:36:36 +0530

    Inside fct and scd processor

--- src/factory_design_pattern/core/fct_processor.py
+++ src/factory_design_pattern/core/fct_processor.py
@@ -3,21 +3,21 @@ from src.factory_design_pattern.utils.spark_singleton import SparkSingleton
 class FactProcessor(BaseProcessor):
     """FACT table ETL"""
 
-
-def __init__(self, table_name, source_path, target_path):
-    self.table_name = table_name
-    self.source_path = source_path
-    self.target_path = target_path
-    self.spark = SparkSingleton.get()
-
-
-def extract(self):
-    return self.spark.read.parquet(self.source_path)
+    def __init__(self, table_name, source_path, target_path):
+        self.table_name = table_name
+        self.source_path = source_path
+        self.target_path = target_path
+        self.spark = SparkSingleton.get()
 
 
-def transform(self, df):
-    return df.dropDuplicates()
+    def extract(self):
+        print("Inside Extract of Fact Processor")
+        return self.spark.read.parquet(self.source_path)
 
 
-def load(self, df):
-    df.write.mode("overwrite").parquet(self.target_path)
+    def transform(self, df):
+        return df.dropDuplicates()
+
+
+    def load(self, df):
+        df.write.mode("overwrite").parquet(self.target_path)
--- src/factory_design_pattern/core/scd_processor.py
+++ src/factory_design_pattern/core/scd_processor.py
@@ -5,20 +5,21 @@ class SCD2Processor(BaseProcessor):
     """SCD2 Implementation class (Polymorphism → overrides BaseProcessor methods)"""
 
 
-def __init__(self, table_name, source_path, target_path):
-    self.table_name = table_name
-    self.source_path = source_path
-    self.target_path = target_path
-    self.spark = SparkSingleton.get()
+    def __init__(self, table_name, source_path, target_path):
+        self.table_name = table_name
+        self.source_path = source_path
+        self.target_path = target_path
+        self.spark = SparkSingleton.get()
 
 
-def extract(self):
-    return self.spark.read.format("delta").load(self.source_path)
+    def extract(self):
+        print("Inside Extract of SCD Processor")
+        return self.spark.read.format("delta").load(self.source_path)
 
 
-def transform(self, df):
-    return df.withColumn("updated_at", current_timestamp())
+    def transform(self, df):
+        return df.withColumn("updated_at", current_timestamp())
 
 
-def load(self, df):
-    df.write.format("delta").mode("append").save(self.target_path)
+    def load(self, df):
+        df.write.format("delta").mode("append").save(self.target_path)
